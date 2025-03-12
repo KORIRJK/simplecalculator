b62d0c3f3e145ab950b043edad8ff33f3fe84183
KORIR JAPHETH KIBET <korirjaphethkibet@gmail.com>
2025-03-12 21:43:50 +0300

    Simple Calculator

---
+++ index.js
@@ -0,0 +1,30 @@
+let num1 = 5
+let num2 = 10
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+let display=document.getElementById("sum-el")
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+function add(){
+    let sum= num1+num2
+    display.textContent=sum
+}
+function subtract(){
+    let sum=num1-num2
+    display.textContent=sum
+}
+function divide(){
+    let sum=num1/num2
+    display.textContent=sum
+}
+function multiply(){
+    let sum=num1*num2
+    display.textContent=sum
+}
