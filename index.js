7ad26f0e96540a9894179b0d8386e600b04fbee3
KORIR JAPHETH KIBET <korirjaphethkibet@gmail.com>
2025-04-20 22:17:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.getElementById("num2-el").textContent = num2
 let display=document.getElementById("sum-el")
 
 //Create four functions: add(), subtract(), divide(), multiply()
-//Call the correct function when the user clicks on  corresponding the buttons
+//Call the correct function when the user clicks on  corresponding the buttons 
 //Perform the given calculation using num1 and  num2 
 //Render the result of the calculation in the paragraph with id="sum-el"
 
